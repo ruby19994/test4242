d589f174e8a24b1453fd0a9ae16c3a527ab026ad
ruby <47886382+ruby19994@users.noreply.github.com>
2019-06-03 02:23:24 +0800

    Add files via upload

---
+++ assets/js/memberbutton.js
@@ -0,0 +1,28 @@
+
+// Get the modal
+var modal = document.getElementById('change_file');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Get the modal
+var modal = document.getElementById('order_list');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Get the modal
+var modal = document.getElementById('comment');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
